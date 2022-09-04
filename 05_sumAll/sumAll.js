d91dfde31030753c5b1323a6befcd442c9503361
kforkamille <kabbarroga@gmail.com>
2022-09-04 11:15:58 +0800

    Corrected sumAll.js and enabled all tests in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let finalSum = 0; 
+    if (typeof a === "number" && typeof b === "number"){ 
+    if (a < b && a>0 && b>0) {  
+     for (i=a; i<=b; i++){ 
+         finalSum += i; 
+     }} else if (a>b && a>0 && b>0) { 
+     for (i=a; i>=b; i--){ 
+         finalSum += i; 
+         } 
+     } else if (a<0||b<0){ 
+         return "ERROR"; 
+     } 
+   return finalSum;  
+ } else { 
+      return "ERROR"; 
+ } 
+ }; 
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
