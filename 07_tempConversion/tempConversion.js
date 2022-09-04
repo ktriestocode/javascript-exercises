c811dea56abdffb83857d6afcf38b0f93693f22f
kforkamille <kabbarroga@gmail.com>
2022-09-04 11:25:48 +0800

    Corrected tempConversion.js and enabled all tests in tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let c = (fahrenheit-32)*(5/9); 
+  let roundedc = Math.round(c*10)/10; 
+  return roundedc 
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let f = (celcius*(9/5)) + 32; 
+  let roundedf = Math.round(f*10)/10; 
+  return roundedf 
 };
 
 // Do not edit below this line
