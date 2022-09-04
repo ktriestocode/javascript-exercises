ec5da92399ee1d84baf03342335098361c8dc5f5
kforkamille <kabbarroga@gmail.com>
2022-09-04 11:03:08 +0800

    Corrected repeatString.js file and enabled tests in the repeatString.spec.js file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) { 
+    let newWord = ''; 
+    if (num < 0) return "ERROR"; 
+    for (i=0; i<num; i++){ 
+        newWord += word; 
+    } 
+return newWord; 
+}; 
 
-};
+
 
 // Do not edit below this line
 module.exports = repeatString;
